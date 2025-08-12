63caae188e65c8904be5ab42b233ad871302e242
Ming Shane Tong <mstong800@gmail.com>
2025-08-13 00:14:59 +1200

    apiserver open api concept

---
+++ services/apiserver/src/LearningPath.Api/Handlers/DefaultApiHandler.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearningPath.Api.Handlers
+{
+	public interface IDefaultApiHandler
+	{
+		public IActionResult PingGet();
+	}
+
+	public class DefaultApiHandler : IDefaultApiHandler
+	{
+		public IActionResult PingGet()
+		{
+			// This method is intentionally left empty.
+			// It serves as a placeholder for the default API handler.
+			return null;
+		}
+	}
+}
---
+++ services/apiserver/src/LearningPath.Api/Handlers/UsersApiHandler.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace LearningPath.Api.Handlers
+{
+	public interface IUsersApiHandler
+	{
+		public IActionResult UsersGet(int? limit, bool? active);
+	}
+
+	public class UsersApiHandler : IUsersApiHandler
+	{
+		public IActionResult UsersGet(int? limit, bool? active)
+		{
+			return null;
+		}
+	}
+}
