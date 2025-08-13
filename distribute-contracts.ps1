96ae26c3d7bae67b443d30aeb642beb261727e05
Ming Shane Tong <mstong800@gmail.com>
2025-08-13 21:22:10 +1200

    web server

--- distribute-contracts.ps1
+++ distribute-contracts.ps1
@@ -1,6 +1,7 @@
 Copy-Item -Path "./contracts/gateway.api.yaml" -Destination "./services/gateway/src/LearningPath.Gateway/Contracts/gateway.api.yaml"
 Copy-Item -Path "./contracts/dag.api.yaml" -Destination "./services/gateway/src/LearningPath.Gateway/Contracts/dag.api.yaml"
 
+Copy-Item -Path "./contracts/gateway.api.yaml" -Destination "./services/webserver/web/contracts/gateway.api.yaml"
 
 # openapi-generator-cli generate -i ./contracts/gateway.api.yaml -g aspnetcore -o ./services/gateway/ -t ./contracts/template/aspnetcore --global-property apis,models,attributes,apiDocs=false,apiTests=false,modelDocs=false,modelTests=false --additional-properties=packageName=LearningPath.Gateway,generateSourceCodeOnly=true
 
