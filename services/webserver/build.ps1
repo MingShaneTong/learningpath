900d8052e1b19f6046dc07700f4c4a9fd0e7fe19
Ming Shane Tong <mstong800@gmail.com>
2025-08-24 00:00:04 +1200

    delete old web

--- services/webserver/build.ps1
+++
@@ -1,5 +0,0 @@
-openapi-generator-cli generate  `
-    -i ./contracts/gateway.api.yaml `
-    -g typescript-fetch `
-    -o ./clients/gateway `
-    --additional-properties=projectName=@learningpath/client-gateway
